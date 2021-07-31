f95aa3d5cae5cbffa44d12f1280900df10a2fea5
[Ben Goff] <[goff.ben@gmail.com]>
2021-07-31 23:51:39 +0100

    Added rate limiting evasion and price checking

--- pricefinder.py
+++ pricefinder.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import json
+import numpy as np
+import time
 
 v = open('variables.json')
-# print(json.load(v))
 historic = json.load(v)
-# print(historic["historicList"])
 uris = [
     "https://uk.pcpartpicker.com/product/vFhmP6/asus-rog-strix-b550-f-gaming-wi-fi-atx-am4-motherboard-rog-strix-b550-f-gaming-wi-fi",
     "https://uk.pcpartpicker.com/product/9nm323/amd-ryzen-53600-36-thz-6-core-processor-100-100000031box",
@@ -18,17 +18,32 @@ uris = [
     "https://uk.pcpartpicker.com/product/ThhmP6/zotac-geforce-rtx-3070-8-gb-gaming-twin-edge-oc-video-card-zt-a30700h-10p"
     ]
 
+def get_random_ua():
+    random_ua = ''
+    ua_file = 'ua_file.txt'
+    try:
+        with open(ua_file) as f:
+            lines = f.readlines()
+            # print(lines)
+        if len(lines) > 0:
+            index = np.random.randint(len(lines)-1)
+            random_ua = lines[index]
+    except Exception as ex:
+        print('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua
 
 def getprice(uri):
+    random_ua = str(get_random_ua().strip())
     r = requests.get(uri, headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"})
+    "User-Agent": random_ua, "referer": "google.co.uk"})
     html = r.text
+    responseCode = r.status_code
+    print("Request: " + uri + "  Response Code: " + str(responseCode))
     soup = BeautifulSoup(html, "html.parser")
     partdetail = {}
-    x = print(soup.find_all("tbody"))
-    print(type(x)
-    if type(x) is not None or len(x) != 0:
-        tds = x[0].find("tr").find_all("td")
+    tds = soup.find_all("tbody")[1].find("tr").find_all("td")
     for td in tds:
         price = td.find("a")
         if price is not None:
@@ -40,6 +55,9 @@ def getprice(uri):
     partdetail["name"] = soup.head.find("meta", property="og:title")["content"]
     print(partdetail)
     listOfPrices.append(partdetail)
+    delays = [7, 4, 6, 2, 10, 19]
+    delay = np.random.choice(delays)
+    time.sleep(delay)
 
 listOfPrices = []
 for uri in uris:
@@ -56,6 +74,15 @@ noOfParts = str(len(listOfPrices))
 totalPrice = str(round(total, 2))
 print("Total Number of Parts: " + noOfParts)
 print("Total Price: £" + totalPrice)
+for historicObj in historic["historicList"]:
+    for currentObj in listOfPrices:
+        # print(historicObj, currentObj)
+        if historicObj == currentObj:
+            if historicObj["price"] == currentObj["price"]:
+                print(historicObj["name"] + ": Same Price")
+            if historicObj["price"] > currentObj["price"]:
+                print("New Low Price for " + historicObj["name"] + ".  Current Price: " + currentObj["price"] + ", Historic Price: " + historicObj["price"])
+
 # with open("variables.json", "w") as stored:
 #     update = {"historicPrice": totalPrice, "noOfParts": noOfParts, "historicList": listOfPrices}
 #     json.dump(update, stored)
