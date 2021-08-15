67e0a15f593ebb8756a93ee4ed2dc3e5a4a3d005
[Ben Goff] <[goff.ben@gmail.com]>
2021-08-15 18:42:41 +0100

    change ua

--- componentscraper.py
+++ componentscraper.py
@@ -145,7 +145,7 @@ class Scraper:
         proxy_host = self.reset_proxy()
         proxy_key = os.getenv('PROXY_KEY')
         http_proxy = "http://" + proxy_key + proxy_host
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"}
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
         proxy_dict = {
             "http": http_proxy
         }
