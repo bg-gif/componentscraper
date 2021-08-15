4ca136b7b740296d798b825452f11069833a1357
[Ben Goff] <[goff.ben@gmail.com]>
2021-08-15 18:18:58 +0100

    running through proxies and with secrets set on env

--- componentscraper.py
+++ componentscraper.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Optional
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -10,30 +9,41 @@ import send_notification
 import os.path
 import os
 from decouple import config
+from urllib.request import Request, urlopen, FancyURLopener
+from urllib.error import URLError
+import urllib3
+from requests.auth import HTTPProxyAuth
+
+
+def write_to_record(historic):
+    os.environ['HISTORIC'] = str(historic)
+    return 'Historic Updated'
 
 
 def price_check(list_of_parts, historic, list_of_prices):
     # Calculate total price of build and list components and prices
     output = "\n\nPart List:\n\n"
     total = 0
+    print(list_of_parts, historic, list_of_prices)
     for part_type in list_of_parts:
         part_name = list_of_prices[part_type]["name"]
-        part_price = list_of_prices[partType]["price"]
-        # print(partName + ": £" + partPrice)
-        historic_low = historic["historic_list"][partType]["historic_low"]
-        output = output + part_name + ": £" + part_price
-        if float(part_price) > float(historic_low):
-            diff = str(round(float(part_price) - float(historic_low)))
-            print(f"{part_type} is £{diff} more expensive than the historic low.")
-        if float(part_price) < float(historic_low):
-            diff = str(round(float(historic_low) - float(part_price), 2))
-            print(f"{part_type} is £{diff} cheaper than the historic low. New Low Price added to historic record")
-            output += ". New Historic Low!\n"
-            send_notification.send_notification_via_pushbullet(f"New {partType} Historic Low Price!",
-                                                              f"---{partType}---\n{list_of_prices[part_type]['name']} has reached a historic low price of £{list_of_prices[part_type]['price']}")
-            historic["historic_list"][part_type]["historic_low"] = part_price
-        else:
-            output += "\n"
+        part_price = list_of_prices[part_type]["price"]
+        if historic != list_of_prices:
+            # print(part_name + ": £" + part_price)
+            historic_low = historic["historic_list"][part_type]["historic_low"]
+            output = output + part_name + ": £" + part_price
+            if float(part_price) > float(historic_low):
+                diff = str(round(float(part_price) - float(historic_low)))
+                print(f"{part_type} is £{diff} more expensive than the historic low.")
+            if float(part_price) < float(historic_low):
+                diff = str(round(float(historic_low) - float(part_price), 2))
+                print(f"{part_type} is £{diff} cheaper than the historic low. New Low Price added to historic record")
+                output += ". New Historic Low!\n"
+                send_notification.send_notification_via_pushbullet(f"New {part_type} Historic Low Price!",
+                                                                   f"---{part_type}---\n{list_of_prices[part_type]['name']} has reached a historic low price of £{list_of_prices[part_type]['price']}")
+                historic["historic_list"][part_type]["historic_low"] = part_price
+            else:
+                output += "\n"
         total = total + float(part_price)
 
     no_of_parts = str(len(list_of_prices))
@@ -45,59 +55,40 @@ def price_check(list_of_parts, historic, list_of_prices):
     logging.debug(output + "\nTotal Number of Parts: " + no_of_parts + "\nTotal Price: £" + total_price)
 
     for part_type in list_of_parts:
-        historic_list = historic["historic_list"]
-        if historic_list[part_type]["name"] != list_of_prices[part_type]["name"]:
-            print(f"\n{part_type} has changed.\n")
-            print(f"{historic_list[part_type]['name']} was changed to {list_of_prices[part_type]['name']}\n")
-            diff = round(float(historic_list[part_type]["price"]) - float(list_of_prices[part_type]["price"]), 2)
-            if diff > 0:
-                print(
-                    f"{list_of_prices[part_type]['name']} is £{str(diff)} cheaper than {historic_list[part_type]['name']}\n")
-            if diff < 0:
-                print(
-                    f"{list_of_prices[part_type]['name']} is £{str(abs(diff))} more expensive than {historic_list[part_type]['name']}\n")
-            historic_list.pop(part_type)
-            historic_list[part_type] = list_of_prices[part_type]
-            changed = True
+        if historic != list_of_prices:
+            historic_list = historic["historic_list"]
+            if historic_list[part_type]["name"] != list_of_prices[part_type]["name"]:
+                print(f"\n{part_type} has changed.\n")
+                print(f"{historic_list[part_type]['name']} was changed to {list_of_prices[part_type]['name']}\n")
+                diff = round(float(historic_list[part_type]["price"]) - float(list_of_prices[part_type]["price"]), 2)
+                if diff > 0:
+                    print(
+                        f"{list_of_prices[part_type]['name']} is £{str(diff)} cheaper than {historic_list[part_type]['name']}\n")
+                if diff < 0:
+                    print(
+                        f"{list_of_prices[part_type]['name']} is £{str(abs(diff))} more expensive than {historic_list[part_type]['name']}\n")
+                historic_list.pop(part_type)
+                historic_list[part_type] = list_of_prices[part_type]
+                changed = True
 
-    if total_price != historic["historic_price"]:
-        print(f"Price Change! New Total Price is £{total_price}\nHistoric Low Price is £{historic['historic_low']}")
-        total_diff = round(float(total_price) - float(historic["historic_price"]), 2)
-        # print(totalDiff)
-        if total_diff < 0:
-            print(f"The new total is £{str(abs(total_diff))} cheaper than previous.\n")
-        if total_diff > 0:
-            print(f"The new total is £{str(total_diff)} more expensive than previous.\n")
-        historic["historic_price"] = str(total_price)
-    if float(total_price) < float(historic["historic_low"]):
-        print("New Historic Low Total Price!")
-        send_notification.send_notification_via_pushbullet("New Historic Low Price!",
-                                                          f"The current cost of the build is £{total_price}\n{output}")
-        historic["historic_low"] = total_price
+            if total_price != historic["historic_price"]:
+                print(
+                    f"Price Change! New Total Price is £{total_price}\nHistoric Low Price is £{historic['historic_low']}")
+                total_diff = round(float(total_price) - float(historic["historic_price"]), 2)
+                # print(total_diff)
+                if total_diff < 0:
+                    print(f"The new total is £{str(abs(total_diff))} cheaper than previous.\n")
+                if total_diff > 0:
+                    print(f"The new total is £{str(total_diff)} more expensive than previous.\n")
+                historic["historic_price"] = str(total_price)
+            if float(total_price) < float(historic["historic_low"]):
+                print("New Historic Low Total Price!")
+                send_notification.send_notification_via_pushbullet("New Historic Low Price!",
+                                                                   f"The current cost of the build is £{total_price}\n{output}")
+                historic["historic_low"] = total_price
+        else:
+            historic = {"historic_list": list_of_prices, "historic_low": total_price, "historic_price": total_price, "historic_no_of_parts": no_of_parts}
     return {"output": output, "no_of_parts": no_of_parts, "total_price": total_price, "historic": historic}
-
-
-def get_part_list(part_sources):
-    list_of_parts = []
-    for part in part_sources:
-        if part.split("-")[0].strip() != "\n":
-            list_of_parts.append(part.split("-")[0].strip())
-    # print(listOfParts)
-    return list_of_parts
-
-
-def get_variables(uris):
-    historic = os.getenv('HISTORIC')
-    if not historic:
-        historic = {}
-    with open(uris) as f:
-        part_sources = f.readlines()
-    return {"part_sources": part_sources, "historic": historic}
-
-
-def write_to_record(historic):
-    os.environ['HISTORIC'] = historic
-    return 'Historic Updated'
 
 
 def get_random_ua():
@@ -117,43 +108,26 @@ def get_random_ua():
         return str(random_ua.strip())
 
 
-def get_price(part, listarg, historic):
-    random_ua = get_random_ua()
-    part_name = part.split("-")[0].strip()
-    part_uri = part.split("-")[1].strip()
-    if part_uri != "\n":
-        r = requests.get(part_uri, headers={"User-Agent": random_ua, "referer": "google.co.uk"})
-    html = r.text
-    response_code = r.status_code
-    print("Request: " + part_uri + "\nResponse Code: " + str(response_code), end="\n")
-    soup = BeautifulSoup(html, "html.parser")
-    part_detail = {}
-    if not soup.find_all("tbody"):
-        print("No Price returned. Most likely due to bot detection", end="\n")
-        return
-    tds = soup.find_all("tbody")[1].find("tr").find_all("td")
-    for td in tds:
-        price = td.find("a")
-        if price is not None:
-            cost = str(price.string)
-            regex = "£\\d{1,3}\\.\\d{2}"
-            if re.match(regex, cost):
-                price_float = cost.split("£")[1]
-                part_detail["price"] = price_float
-                part_detail["link"] = "https://uk.partpicker.com" + price["href"]
-                if historic["historic_list"][part_name]:
-                    historic_part = historic["historic_list"][part_name]
-                    if float(historic_part["historic_low"]) < float(price_float):
-                        part_detail["historic_low"] = historic_part["historic_low"]
-                    else:
-                        part_detail["historic_low"] = price_float
-    part_detail["name"] = soup.head.find("meta", property="og:title")["content"]
-    # print(part_detail)
-    listarg[partName] = part_detail
-    delays = range(20)
-    delay = np.random.choice(delays)
-    time.sleep(delay)
-    return "All Prices Collected"
+def get_part_list(part_sources):
+    list_of_parts = []
+    for part in part_sources:
+        if part.split("-")[0].strip() != "\n":
+            list_of_parts.append(part.split("-")[0].strip())
+    # print(list_of_parts)
+    return list_of_parts
+
+
+def get_variables(uris):
+    historic = json.loads(os.getenv('HISTORIC'))
+    with open(uris) as f:
+        part_sources = f.readlines()
+    return {"part_sources": part_sources, "historic": historic}
+
+
+def get_proxy_list():
+    proxy_str = os.getenv('PROXY_LIST')
+    proxy_list = proxy_str.split(',')
+    return proxy_list
 
 
 class Scraper:
@@ -161,14 +135,91 @@ class Scraper:
     def __init__(self, uris):
         self.part_sources = get_variables(uris)["part_sources"]
         self.historic = get_variables(uris)["historic"]
+        self.proxy_list = get_proxy_list()
+
+    def reset_proxy(self):
+        index = np.random.randint(len(self.proxy_list) - 1)
+        proxy_host = self.proxy_list[index]
+        return proxy_host
+
+    def make_request(self, uri):
+        proxy_host = self.reset_proxy()
+        proxy_key = os.getenv('PROXY_KEY')
+        http_proxy = "http://" + proxy_key + proxy_host
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"}
+        proxy_dict = {
+            "http": http_proxy
+        }
+        r = requests.get(uri, headers=headers, proxies=proxy_dict)
+        return r
+
+    def get_price(self, part, part_list, historic):
+        part_name = part.split("-")[0].strip()
+        part_uri = part.split("-")[1].strip()
+        if part_uri != "\n":
+            try:
+                r = self.make_request(part_uri)
+                response_code = r.status_code
+                print(response_code)
+            except URLError as e:
+                print(e)
+                self.get_price(part, part_list, historic)
+            html = r.content.decode('utf-8')
+        print("Request: " + part_uri + "\nResponse Code: " + str(response_code), end="\n")
+        soup = BeautifulSoup(html, "html.parser")
+        part_detail = {}
+        if not soup.find_all("tbody"):
+            print("No Price returned. Most likely due to bot detection", end="\n")
+            return 400
+        tds = soup.find_all("tbody")[1].find("tr").find_all("td")
+        for td in tds:
+            price = td.find("a")
+            if price is not None:
+                cost = str(price.string)
+                regex = "£\\d{1,3}\\.\\d{2}"
+                if re.match(regex, cost):
+                    price_float = cost.split("£")[1]
+                    part_detail["price"] = price_float
+                    part_detail["link"] = "https://uk.partpicker.com" + price["href"]
+                    if historic != {}:
+                        if historic.historic_list[part_name]:
+                            historicPart = historic["historic_list"][part_name]
+                            if float(historicPart["historic_low"]) < float(price_float):
+                                part_detail["historic_low"] = historicPart["historic_low"]
+                            else:
+                                part_detail["historic_low"] = price_float
+        part_detail["name"] = soup.head.find("meta", property="og:title")["content"]
+        # print(part_detail)
+        part_list[part_name] = part_detail
+        delays = range(5)
+        delay = np.random.choice(delays)
+        time.sleep(delay)
+        return response_code
 
     def get_list_of_prices(self, part_sources, historic):
         # Create list of all component current details
         list_of_prices = {}
+        all_good = True
+        response_list = []
         for part in part_sources:
-            get_price(part, list_of_prices, historic)
+            rc = self.get_price(part, list_of_prices, historic)
+            response_list.append({part: rc})
+            if rc != 200:
+                all_good = False
         if historic == {}:
-            self['historic'] = list_of_prices
-        # print(listOfPrices)
+            self.historic = list_of_prices
+        # print(list_of_prices)
         print("\n")
-        return list_of_prices
+        return {"list_of_prices": list_of_prices, "all_good": all_good, 'response_list': response_list}
+
+
+class AppURLopener(FancyURLopener):
+    # random_ua = get_random_ua()
+    version = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"
+
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        if errcode == 403:
+            raise ValueError("403")
+        return super(FixFancyURLOpener, self).http_error_default(
+            url, fp, errcode, errmsg, headers
+        )
--- pricefinder.py
+++ pricefinder.py
@@ -5,11 +5,11 @@ def run():
     v = cs.Scraper("uris.txt")
     part_list = cs.get_part_list(v.part_sources)
     get_list = v.get_list_of_prices(v.part_sources, v.historic)
-    list_of_prices = get_list
+    list_of_prices = get_list['list_of_prices']
     if not get_list['all_good']:
         print("Scraping Failed")
-        return {'message': 'Something fucked up', 'responses': get_list['responseList']}
+        return {'message': 'Something fucked up', 'responses': get_list['response_list']}
     price_check = cs.price_check(part_list, v.historic, list_of_prices)
     v.historic = price_check["historic"]
     cs.write_to_record(v.historic)
-    return {"list_of_prices": listOfPrices, "historic": v.historic, "part_list": partList}
+    return {"list_of_prices": list_of_prices, "historic": v.historic, "part_list": part_list}
